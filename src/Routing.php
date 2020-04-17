d23137afd8254547ae58923da1ece29048d6087d
神谷友樹 <jetree900@gmail.com>
2020-04-17 21:09:12 +0900

    チャンネルへの投稿時のルーティングを追加

--- src/Routing.php
+++ src/Routing.php
@@ -9,9 +9,12 @@ class Routing
     if($data["type"] === 'url_verification'){
       return UrlVerification::class;
     };
+
+    if(isset($data["event"]["channel"])){
+      return WhetherToProtect::class;
+    }
   }
 
-  
   public static function run($data)
   {
     self::exec($data)::run($data);
